ec92c929ffefef963ff2f0fac518cacbb7d3833e
Boissier Florian <boissierflorian@outlook.com>
2017-11-16 13:26:57 +0100

    Tâche 4

--- src/org/ulco/Group.java
+++ src/org/ulco/Group.java
@@ -9,7 +9,7 @@ public class Group extends GraphicsObject {
 
     public Group() {
         m_objectList = new Vector<>();
-        m_ID = ++ID.ID;
+        m_ID = ID.getInstance().generateId();
     }
 
     public Group(String json) {
--- src/org/ulco/ID.java
+++ src/org/ulco/ID.java
@@ -1,5 +1,28 @@
 package org.ulco;
 
 public class ID {
-    static public int ID = 0;
+    private static ID instance;
+    private int id;
+
+
+    private ID() {
+        this.id = 0;
+    }
+
+    public int generateId() {
+        return ++this.id;
+    }
+
+    public int getOldId() {
+        return this.id;
+    }
+
+    public static ID getInstance() {
+        if (instance == null) {
+            instance = new ID();
+        }
+
+        return instance;
+    }
+
 }
